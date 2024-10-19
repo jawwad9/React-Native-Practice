1b3bfa8e6f12b3e5dd34dde1d3cd964a462cdac7
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-19 17:58:38 +0500

    todo input complete

--- todo-app/app/index.tsx
+++ todo-app/app/index.tsx
@@ -1,22 +1,54 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+
 
 const index = () => {
+  const [inputVal, setInputVal] = useState('');
+
+
+  const addTodo = () => {
+    console.log(inputVal);
+    
+  }
+
   return (
-    <View style={style.container}>
-      <Text style={{
-        fontSize: 30,
-      }}>Todo App</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.todoTitle}>Todo App</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setInputVal}
+        value={inputVal}
+        placeholder='Enter Todo '
+      />
+      <TouchableOpacity style={styles.button} onPress={addTodo}>
+        <Text>Click</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
-const style = StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+todoTitle: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginVertical: 10
+  },
+  input: {
+    height: 40,
+    marginHorizontal: 30,
+    marginVertical : 20,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
     alignItems: 'center',
-  }
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginHorizontal: 170,
+  },
 })
 
 export default index
