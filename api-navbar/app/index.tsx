87d8d195b807dd9b9826498444a07c6b7afe7d41
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-24 23:52:58 +0500

    complete

--- api-navbar/app/index.tsx
+++ api-navbar/app/index.tsx
@@ -11,7 +11,6 @@ export default function Index() {
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
