fcb1a9d88d401bdf8a3808f3e7d03b4b4634a7fd
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-24 23:56:20 +0500

    commit

--- api-navbar/app/index.tsx
+++ api-navbar/app/index.tsx
@@ -10,7 +10,6 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
